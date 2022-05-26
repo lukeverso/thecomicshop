6b16d1eedb67b7fd909c71026fb44c1ceeebb8d7
lukeverso <cristhovamlucas@gmail.com>
2022-05-26 11:57:49 -0300

    Quinto commit

--- server.js
+++ server.js
@@ -29,7 +29,9 @@ const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@lukebase.oi3mn.mongodb.net/?retryWrites=true&w=majority`)
      .then(() => {
           console.log("Conectado ao MongoDB.")
-          app.listen(port);
+          app.listen(port, () => {
+               console.log(`Aplicação funcionando em http://localhost:${port}`);
+          })
      })
      .catch((err) => {
           console.log(err);
