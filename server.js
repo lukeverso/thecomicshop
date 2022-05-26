b8768ee351f65cac9168bde32c45ef7f544f84ee
lukeverso <cristhovamlucas@gmail.com>
2022-05-26 12:04:33 -0300

    Sexto commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@lukebase.oi3mn.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://lukeverso:JBrDhAtNWNdN8BFj@lukebase.oi3mn.mongodb.net/?retryWrites=true&w=majority`)
      .then(() => {
           console.log("Conectado ao MongoDB.")
           app.listen(port, () => {
